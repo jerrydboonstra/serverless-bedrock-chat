b8a17a671e50e94d35fa1ec0afd054cde19978e3
Jerry Boonstra <jerry@obexmetrics.com>
2024-05-31 14:54:09 -0700

    use region with create bucket

--- backend/createDeploymentBucket.sh
+++ backend/createDeploymentBucket.sh
@@ -2,7 +2,7 @@ aws s3api head-bucket --bucket $BE_DEPLOYMENT_BUCKET || not_exist=true
 if [ $not_exist ]; then
   echo "Creating Lambda deployment bucket"
   echo $DEPLOYMENT_BUCKET
-  aws s3api create-bucket --acl private --bucket $BE_DEPLOYMENT_BUCKET
+  aws s3api create-bucket --acl private --bucket $BE_DEPLOYMENT_BUCKET --create-bucket-configuration LocationConstraint=$AWS_REGION
 else
   echo "Lambda deployment bucket exists"
 fi
